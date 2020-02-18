6015e613d174009165bd4980f1b615e430cce146
L8rlTinkerer <nickumcb@gmail.com>
2020-02-19 09:45:13 +1300

    Added CORS error handling

--- app.js
+++ app.js
@@ -12,6 +12,18 @@ const enrolmentRoutes = require('./api/routes/enrolments');
 app.use(morgan('dev')); // console logging
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 
 // activate requests/routes middleware
